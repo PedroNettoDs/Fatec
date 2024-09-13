2e162917347000710a8d4dd1e84030f54ffd1466
Pedro Henrique Netto dos Santos <99490297+PedroNettoDs@users.noreply.github.com>
2024-09-13 00:07:44 -0300

    Organização

---
+++ ALGORTIMOS/2.Condicionais/Equação de segundo grau e tipo raiz.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // declarando variáveis
+    float a, b, c, delta, x1, x2;
+    
+    // exibição e entrada
+    printf("Insira os coeficientes\nA: ");
+    scanf("%f", &a);
+    printf("B: ");
+    scanf("%f", &b);
+    printf("C: ");
+    scanf("%f", &c);
+
+    // processamento
+    if (a != 0) {
+        delta = (b * b) - (4 * a * c);
+        if (delta < 0) {
+            printf("A equacao nao possui raizes reais.\n");
+        } else if (delta == 0) {
+            x1 = -b / (2 * a);
+            printf("A equacao possui uma raiz real: %.2f\n", x1);
+        } else {
+            x1 = (-b + sqrt(delta)) / (2 * a);
+            x2 = (-b - sqrt(delta)) / (2 * a);
+            printf("A equacao possui duas raizes reais: %.2f e %.2f\n", x1, x2);
+        }
+    } else {
+        printf("Nao e equacao de segundo grau.\n");
+    }
+
+    return 0;
+}
+
---
+++ ALGORTIMOS/2.Condicionais/Numero inteiro, igual ou maior.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // declarando variáveis
+    int number;
+    
+    // entrada
+    printf("Detector de numeros 2000\nInforme um numero: ");
+    scanf("%d", &number);
+    printf("Processando...\n");
+    
+    // processamento
+    if (number > 0) {
+        printf("O numero e maior que 0\n");
+    } else if (number == 0) {
+        printf("O numero e igual a 0\n");
+    } else {
+        printf("O numero e menor que 0\n");
+    }
+
+    return 0;
+}
+
