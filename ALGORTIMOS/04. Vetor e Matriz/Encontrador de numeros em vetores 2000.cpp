4b2b7834cd12904849b90a17e69e6e6c5b9f9e21
Pedro Henrique Netto dos Santos <99490297+PedroNettoDs@users.noreply.github.com>
2024-10-10 10:43:48 -0300

    Vetor e Matriz

---
+++ ALGORTIMOS/04. Vetor e Matriz/Encontrador de numeros em vetores 2000.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define QTDL 101
+
+int main (){
+	int number[QTDL], posicao[QTDL];
+	int i, escolha, continuar;
+	int p;
+  	srand(time(0));
+  	
+  	do {
+  		p = 0;
+  		printf ("Encontrador de numeros em vetores 2000\nPrimeiro, sera gerado numeracoes aleatorias.\n");
+  		sleep (2);
+ 		printf ("Agora informe o numero que deseja encontrar: ");
+		scanf ("%d", &escolha);
+		sleep (2);
+
+		for (i=0; i<QTDL; i++){ 
+			number[i] = (rand() % 100);
+			printf ("n[%d]: %d \n", i, number[i]);
+			
+			if (number[i] == escolha){
+				 posicao[p] = i+1;
+				 p++;
+			}
+		}
+		
+  		sleep (3);
+		
+		if (p > 0){
+			printf ("\n\nO numero %d apareceu nas seguintes posicoes:\n", escolha);
+			for (i=0; i<p; i++){	
+				printf("Posicao: %d\n", posicao[i]);
+			}
+		} else {
+			printf ("\n\nO numero nao apareceu :(");
+		}
+		
+		printf ("\nTentar novamente? (1 = Sim, 0 = Nao): ");
+		scanf("%d", &continuar);
+		printf ("\n\n");
+		
+	} while (continuar == 1);
+	
+	printf ("Obrigado!\n");
+	return 0;
+}
+
